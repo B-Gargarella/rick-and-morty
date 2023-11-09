bda0df5dd54565463470d69f058139169a9f267a
bruno <b.gargarella@gmail.com>
2023-11-09 09:48:22 +0100

    Implementation.

--- data/src/main/java/com/bgargarella/ram/data/api/APIService.kt
+++ data/src/main/java/com/bgargarella/ram/data/api/APIService.kt
@@ -4,6 +4,9 @@ import com.bgargarella.ram.data.base.model.BaseResponse
 import com.bgargarella.ram.data.character.model.CharacterResponse
 import com.bgargarella.ram.data.episode.model.EpisodeResponse
 import com.bgargarella.ram.data.location.model.LocationResponse
+import com.bgargarella.ram.domain.character.model.Character
+import com.bgargarella.ram.domain.episode.model.Episode
+import com.bgargarella.ram.domain.location.model.Location
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
@@ -13,33 +16,33 @@ interface APIService {
 
     @GET("character")
     suspend fun getCharacters(
-        @Query("page") page: Int
+        @Query("page") page: Int,
     ): Response<BaseResponse<CharacterResponse>>
 
     @GET("character/{ids}")
     suspend fun getCharacters(
         @Path("ids") ids: List<Int>,
-    ): Response<List<CharacterResponse>>
+    ): Response<List<Character>>
 
     @GET("character")
     suspend fun getCharacter(
-        @Query("id") id: Int
-    ): Response<CharacterResponse>
+        @Query("id") id: Int,
+    ): Response<Character>
 
     @GET("episode")
     suspend fun getEpisodes(
-        @Query("page") page: Int
+        @Query("page") page: Int,
     ): Response<BaseResponse<EpisodeResponse>>
 
     @GET("episode/{ids}")
     suspend fun getEpisodes(
         @Path("ids") ids: List<Int>,
-    ): Response<List<EpisodeResponse>>
+    ): Response<List<Episode>>
 
     @GET("episode")
     suspend fun getEpisode(
-        @Query("id") id: Int
-    ): Response<EpisodeResponse>
+        @Query("id") id: Int,
+    ): Response<Episode>
 
     @GET("location")
     suspend fun getLocations(
@@ -49,11 +52,10 @@ interface APIService {
     @GET("location/{ids}")
     suspend fun getLocations(
         @Path("ids") ids: List<Int>,
-        @Query("page") page: Int,
-    ): Response<BaseResponse<LocationResponse>>
+    ): Response<List<Location>>
 
     @GET("location")
     suspend fun getLocation(
         @Query("id") id: Int
-    ): Response<LocationResponse>
+    ): Response<Location>
 }
