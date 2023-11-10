b7e56fd5edb71ce2cc845c1f471284cc9237f188
bruno <b.gargarella@gmail.com>
2023-11-10 15:32:32 +0100

    Implementation.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,13 +42,18 @@ android {
             )
         }
     }
+
+    val javaVersion = extra["java_version"] as JavaVersion
+
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = javaVersion
+        targetCompatibility = javaVersion
     }
+
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = javaVersion.toString()
     }
+
     buildToolsVersion = extra["build_tools_version"] as String
 }
 
@@ -59,12 +64,8 @@ dependencies {
 
     val daggerHiltVersion = rootProject.extra["dagger_hilt_version"]
     implementation("com.google.dagger:hilt-android:$daggerHiltVersion")
-    ksp("com.google.dagger:hilt-compiler:$daggerHiltVersion")
-    // ksp("com.google.dagger:hilt-android-compiler:$daggerHiltVersion")
+    ksp("com.google.dagger:hilt-android-compiler:$daggerHiltVersion")
 
-    // val navigationVersion = rootProject.extra["navigation_version"]
-    // implementation("androidx.navigation:navigation-compose:$navigationVersion")
-    // implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
     implementation("androidx.activity:activity-compose:1.8.0")
 
     val retrofitVersion = rootProject.extra["retrofit_version"]
@@ -72,17 +73,13 @@ dependencies {
     implementation("com.squareup.retrofit2:converter-moshi:$retrofitVersion")
 
     val okhttp3Version = rootProject.extra["okhttp3_version"]
-    // implementation("com.squareup.okhttp3:okhttp:$okhttp3Version")
     implementation("com.squareup.okhttp3:logging-interceptor:$okhttp3Version")
 
-    implementation("com.squareup.moshi:moshi-kotlin:${rootProject.extra["moshi_kotlin_version"]}")
+    val moshiKotlinVersion = rootProject.extra["moshi_kotlin_version"]
+    implementation("com.squareup.moshi:moshi-kotlin:$moshiKotlinVersion")
 
     val roomVersion = rootProject.extra["room_version"]
-    // To use Kotlin Symbol Processing (KSP)
-    ksp("androidx.room:room-compiler:$roomVersion")
-    // optional - Kotlin Extensions and Coroutines support for Room
     implementation("androidx.room:room-ktx:$roomVersion")
-    // implementation("androidx.room:room-paging:$roomVersion")
 
     testImplementation("junit:junit:${rootProject.extra["junit_version"]}")
     androidTestImplementation("androidx.test.ext:junit:${rootProject.extra["ext_junit_version"]}")
