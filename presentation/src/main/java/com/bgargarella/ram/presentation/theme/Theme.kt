06886f9d5e470fd14ac819e34b1eeca13e382412
bruno <b.gargarella@gmail.com>
2023-12-08 12:22:51 +0100

    Implementation.

--- presentation/src/main/java/com/bgargarella/ram/presentation/theme/Theme.kt
+++ presentation/src/main/java/com/bgargarella/ram/presentation/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.bgargarella.ram.presentation.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -10,10 +9,10 @@ import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.SideEffect
-import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.WindowCompat
+import androidx.compose.ui.tooling.preview.Preview
+import com.google.accompanist.systemuicontroller.rememberSystemUiController
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple40,
@@ -30,7 +29,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun RamAppTheme(
+fun AppTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
@@ -49,15 +48,14 @@ fun RamAppTheme(
         darkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
     val view = LocalView.current
     if (!view.isInEditMode) {
+        val systemUiController = rememberSystemUiController()
         SideEffect {
-            WindowCompat.getInsetsController(
-                (view.context as Activity).window.apply {
-                    statusBarColor = colorScheme.primary.toArgb()
-                },
-                view
-            ).isAppearanceLightStatusBars = darkTheme
+            systemUiController.setStatusBarColor(
+                color = colorScheme.primaryContainer,
+            )
         }
     }
 
@@ -66,5 +64,51 @@ fun RamAppTheme(
         typography = Typography,
         shapes = Shapes,
         content = content
+    )
+}
+
+@Preview(showSystemUi = true, showBackground = true)
+@Composable
+fun AppThemePreview() {
+    val darkTheme: Boolean = isSystemInDarkTheme()
+    // Dynamic color is available on Android 12+
+    val dynamicColor = true
+
+    val colorScheme = when {
+        dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
+            val context = LocalContext.current
+            if (darkTheme) {
+                dynamicDarkColorScheme(context)
+            } else {
+                dynamicLightColorScheme(context)
+            }
+        }
+
+        darkTheme -> DarkColorScheme
+        else -> LightColorScheme
+    }
+
+    val view = LocalView.current
+    if (!view.isInEditMode) {
+        val systemUiController = rememberSystemUiController()
+        SideEffect {
+            systemUiController.setStatusBarColor(
+                color = colorScheme.secondaryContainer,
+            )
+        }
+    }
+
+    AppTheme(
+        darkTheme = isSystemInDarkTheme(),
+        // Dynamic color is available on Android 12+
+        dynamicColor = true,
+        content = {
+            MaterialTheme(
+                colorScheme = colorScheme,
+                typography = Typography,
+                shapes = Shapes,
+                content = {},
+            )
+        }
     )
 }
