d51a5ba2feade1dde5c33ac03bef8520ea5266ab
Bruno <bruno.gargarella.contractor@bbva.com>
2024-10-10 11:41:47 +0200

    Implementation.

--- presentation/src/main/java/com/bgargarella/ram/presentation/base/screen/RowDivider.kt
+++ presentation/src/main/java/com/bgargarella/ram/presentation/base/screen/RowDivider.kt
@@ -2,7 +2,7 @@ package com.bgargarella.ram.presentation.base.screen
 
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.material3.Divider
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -10,10 +10,10 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun RowDivider() {
-    Divider(
-        color = Color(0xFFE6EAF5),
+    HorizontalDivider(
         modifier = Modifier
             .fillMaxWidth()
-            .height(1.dp)
+            .height(1.dp),
+        color = Color(0xFFE6EAF5)
     )
 }
