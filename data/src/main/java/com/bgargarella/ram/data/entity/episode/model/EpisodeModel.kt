cea4ce6599b133043124d0f6b9ecf48b8fd2087a
Bruno <bruno.gargarella.contractor@bbva.com>
2024-10-10 11:43:08 +0200

    Implementation.

--- data/src/main/java/com/bgargarella/ram/data/entity/episode/model/EpisodeModel.kt
+++ data/src/main/java/com/bgargarella/ram/data/entity/episode/model/EpisodeModel.kt
@@ -1,13 +1,14 @@
-package com.bgargarella.ram.data.episode.model
+package com.bgargarella.ram.data.entity.episode.model
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import com.bgargarella.ram.data.base.model.BaseModel
+import com.bgargarella.ram.data.entity.base.model.BaseModel
+import com.bgargarella.ram.data.util.EPISODE
 
-@Entity(tableName = "episode")
+@Entity(tableName = EPISODE)
 data class EpisodeModel(
     @PrimaryKey override val id: Int,
-    val name: String,
+    override val name: String,
     val releaseDate: String,
     val code: String,
     val characters: List<Int>,
