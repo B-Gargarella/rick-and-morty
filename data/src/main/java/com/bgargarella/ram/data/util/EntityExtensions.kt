893926819c85c4395e6972219cf1a9779ddb5899
Bruno <bruno.gargarella.contractor@bbva.com>
2024-10-10 12:03:44 +0200

    Implementation.

--- data/src/main/java/com/bgargarella/ram/data/util/EntityExtensions.kt
+++ data/src/main/java/com/bgargarella/ram/data/util/EntityExtensions.kt
@@ -2,11 +2,15 @@ package com.bgargarella.ram.data.util
 
 import com.bgargarella.ram.domain.BuildConfig
 
-fun String.getIdFromCharacter(): Int = getIdFromUrl(this, "character")
+const val CHARACTER: String = "character"
+const val EPISODE: String = "episode"
+const val LOCATION: String = "location"
 
-fun String.getIdFromEpisode(): Int = getIdFromUrl(this, "episode")
+fun String.getIdFromCharacter(): Int = getIdFromUrl(this, CHARACTER)
 
-fun String.getIdFromLocation(): Int = getIdFromUrl(this, "location")
+fun String.getIdFromEpisode(): Int = getIdFromUrl(this, EPISODE)
+
+fun String.getIdFromLocation(): Int = getIdFromUrl(this, LOCATION)
 
 private fun getIdFromUrl(url: String, prefix: String): Int =
     url.removePrefix("${BuildConfig.BASE_URL}$prefix/").toDouble().toInt()
