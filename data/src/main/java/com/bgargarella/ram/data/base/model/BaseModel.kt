de9287d029956df5c29e25682b873f26f67c5a08
Bruno <bruno.gargarella.contractor@bbva.com>
2024-10-10 12:06:30 +0200

    Implementation.

--- data/src/main/java/com/bgargarella/ram/data/base/model/BaseModel.kt
+++
@@ -1,5 +0,0 @@
-package com.bgargarella.ram.data.base.model
-
-interface BaseModel {
-    val id: Int
-}
---
+++ data/src/main/java/com/bgargarella/ram/data/entity/base/model/BaseModel.kt
@@ -0,0 +1,6 @@
+package com.bgargarella.ram.data.entity.base.model
+
+interface BaseModel {
+    val id: Int
+    val name: String
+}
